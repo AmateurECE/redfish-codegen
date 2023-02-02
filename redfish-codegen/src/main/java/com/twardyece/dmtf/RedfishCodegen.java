f96703244006ebce97b24be34bc1be3df4cdceb2
Ethan D. Twardy <ethan.twardy@gmail.com>
2023-02-01 18:17:52 -0600

    Complete maven setup and implement command line interface

--- redfish-codegen/src/main/java/com/twardyece/dmtf/RedfishCodegen.java
+++ redfish-codegen/src/main/java/com/twardyece/dmtf/RedfishCodegen.java
@@ -1,5 +1,7 @@
 package com.twardyece.dmtf;
 
+import org.apache.commons.cli.*;
+
 /**
  * Redfish Code Generator for the Rust language
  * Based on swagger-parser v3
@@ -8,6 +10,30 @@ public class RedfishCodegen
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Option apiDirectoryOption = new Option("apiDirectory", true, "Directory containing openapi resource files");
+        apiDirectoryOption.setRequired(true);
+        Option crateDirectoryOption = new Option("crateDirectory", true, "Directory containing Cargo.toml, into which output sources are written");
+        crateDirectoryOption.setRequired(true);
+
+        Options options = new Options();
+        options.addOption(apiDirectoryOption);
+        options.addOption(crateDirectoryOption);
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+
+        try {
+            CommandLine command = parser.parse(options, args);
+
+            String apiDirectory = command.getOptionValue("apiDirectory");
+            String crateDirectory = command.getOptionValue("crateDirectory");
+
+            System.out.println(apiDirectory);
+            System.out.println(crateDirectory);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            formatter.printHelp("RedfishCodegen", options);
+            System.exit(1);
+        }
     }
 }
