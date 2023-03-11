555f36858727241afafcb99e16839df5e34ffc5d
Ethan D. Twardy <ethan.twardy@gmail.com>
2023-03-11 00:20:06 -0600

    The proc macro almost works...

--- into-redfish-message/src/lib.rs
+++ into-redfish-message/src/lib.rs
@@ -17,13 +17,156 @@
 use proc_macro2::TokenStream;
 use quote::{quote, quote_spanned};
 use syn::spanned::Spanned;
-use syn::{
-    parse_macro_input, Data, DeriveInput, Field, Fields, Lit, Meta, NestedMeta,
-};
+use syn::{Attribute, DeriveInput, Lit, Meta, MetaNameValue, NestedMeta};
 
-#[proc_macro_derive(IntoRedfishMessage)]
-pub fn derive_into_redfish_message(
-    input: proc_macro::TokenStream,
-) -> proc_macro::TokenStream {
-    todo!()
+const NOT_LIST_ERROR: &str = "attribute requires a list of key/value pairs";
+const MESSAGE_IDENT: &str = "message";
+
+#[derive(Default)]
+struct VariantContext {
+    message: Option<syn::LitStr>,
+    id: Option<syn::LitStr>,
+    severity: Option<proc_macro2::Literal>,
+    resolution: Option<syn::LitStr>,
+}
+
+fn attribute_list<'a, I>(mut attributes: I, ident: &str) -> Vec<Meta>
+where
+    I: Iterator<Item = &'a Attribute>,
+{
+    attributes
+        .find(|a| a.path.is_ident(ident))
+        .and_then(|a| {
+            let meta = a.parse_meta().expect("Failed to parse attribute");
+            if let Meta::List(ref list) = meta {
+                Some(
+                    list.nested
+                        .iter()
+                        .filter_map(|nested| match nested {
+                            NestedMeta::Meta(ref value) => Some(value.clone()),
+                            _ => {
+                                panic!("Expecting a structured attribute")
+                            }
+                        })
+                        .collect::<Vec<_>>(),
+                )
+            } else {
+                panic!("Expecting a list of nested attributes")
+            }
+        })
+        .unwrap_or_else(|| panic!("No attribute matching identifier \"{}\"", ident))
+}
+
+fn attribute_path_list<'a, I>(attributes: I, ident: &str) -> Vec<syn::Path>
+where
+    I: Iterator<Item = &'a Attribute>,
+{
+    attribute_list(attributes, ident)
+        .into_iter()
+        .filter_map(|meta| match meta {
+            Meta::Path(ref path) => Some(path.clone()),
+            _ => {
+                panic!("{}", NOT_LIST_ERROR)
+            }
+        })
+        .collect::<Vec<_>>()
+}
+
+// Obtain an iterator over the list of nested Meta attributes of a field
+fn attribute_name_value_list<'a, I>(attributes: I, ident: &str) -> Vec<MetaNameValue>
+where
+    I: Iterator<Item = &'a Attribute>,
+{
+    attribute_list(attributes, ident)
+        .into_iter()
+        .filter_map(|meta| match meta {
+            Meta::NameValue(ref name_value) => Some(name_value.clone()),
+            _ => {
+                panic!("{}", NOT_LIST_ERROR)
+            }
+        })
+        .collect::<Vec<_>>()
+}
+
+fn get_message_type(input: &DeriveInput) -> TokenStream {
+    let attributes = attribute_path_list(input.attrs.iter(), MESSAGE_IDENT);
+    let message_type = attributes.into_iter().last().unwrap();
+    quote!(#message_type)
+}
+
+fn define_variant_coersion(data: &syn::Data, message_type: &TokenStream) -> TokenStream {
+    match *data {
+        syn::Data::Enum(ref data) => {
+            let variants = data.variants.iter().map(|variant| {
+                let name = &variant.ident;
+                let mut context = VariantContext {
+                    ..Default::default()
+                };
+
+                for attribute in attribute_name_value_list(variant.attrs.iter(), MESSAGE_IDENT) {
+                    if attribute.path.is_ident("message") {
+                        match attribute.lit {
+                            Lit::Str(ref value) => context.message = Some(value.clone()),
+                            _ => panic!("{}", NOT_LIST_ERROR),
+                        }
+                    } else if attribute.path.is_ident("id") {
+                        match attribute.lit {
+                            Lit::Str(ref value) => context.id = Some(value.clone()),
+                            _ => panic!("{}", NOT_LIST_ERROR),
+                        }
+                    } else if attribute.path.is_ident("severity") {
+                        match attribute.lit {
+                            Lit::Verbatim(ref value) => context.severity = Some(value.clone()),
+                            _ => panic!("{}", NOT_LIST_ERROR),
+                        }
+                    } else if attribute.path.is_ident("resolution") {
+                        match attribute.lit {
+                            Lit::Str(ref value) => context.resolution = Some(value.clone()),
+                            _ => panic!("{}", NOT_LIST_ERROR),
+                        }
+                    } else {
+                        panic!("{}", NOT_LIST_ERROR)
+                    }
+                }
+
+                quote_spanned! {
+                    variant.span() => Self::#name => #message_type {
+                        // message: Some(#(context.message)),
+                        // message_id: Some(#(context.id)),
+                        // message_severity: Some(#(context.severity)),
+                        // resolution: Some(#(context.resolution)),
+                        ..Default::default()
+                    }
+                }
+            });
+
+            quote! { #(#variants ,)* }
+        }
+
+        syn::Data::Struct(_) | syn::Data::Union(_) => unimplemented!(),
+    }
+}
+
+#[proc_macro_derive(IntoRedfishMessage, attributes(message))]
+pub fn derive_into_redfish_message(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let input = syn::parse_macro_input!(input as DeriveInput);
+    let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
+
+    let name = &input.ident;
+    let message_type = get_message_type(&input);
+    let variant_coersion = define_variant_coersion(&input.data, &message_type);
+    let expanded = quote! {
+        // The generated impl
+        impl #impl_generics Into< #message_type > for #name #ty_generics
+           #where_clause
+        {
+           fn into(self) -> #message_type {
+               match self {
+                   #variant_coersion
+               }
+           }
+        }
+    };
+
+    proc_macro::TokenStream::from(expanded)
 }
