3acaca7d8168738ef0a5832d2a0e012702f0348a
Ethan D. Twardy <ethan.twardy@gmail.com>
2023-03-10 21:38:56 -0600

    Begin filling out into-redfish-message

--- into-redfish-message/src/lib.rs
+++ into-redfish-message/src/lib.rs
@@ -1,14 +1,29 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+// Author: Ethan D. Twardy <ethan.twardy@gmail.com>
+//
+// Copyright 2023, Ethan Twardy. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the \"License\");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an \"AS IS\" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+use proc_macro2::TokenStream;
+use quote::{quote, quote_spanned};
+use syn::spanned::Spanned;
+use syn::{
+    parse_macro_input, Data, DeriveInput, Field, Fields, Lit, Meta, NestedMeta,
+};
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[proc_macro_derive(IntoRedfishMessage)]
+pub fn derive_into_redfish_message(
+    input: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    todo!()
 }
