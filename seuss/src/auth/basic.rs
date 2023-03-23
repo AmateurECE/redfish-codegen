5a8e3b0e2f1f5a6f7c55a05c61dec82f38427750
Ethan D. Twardy <ethan.twardy@gmail.com>
2023-03-23 18:27:46 -0500

    Minimize detail in case of authorization failure

--- seuss/src/auth/basic.rs
+++ seuss/src/auth/basic.rs
@@ -21,12 +21,8 @@ use base64::engine::{general_purpose, Engine};
 use redfish_codegen::{models::redfish, registries::base::v1_15_0::Base};
 use std::str;
 
-fn unauthorized(uri: &Uri, message: String) -> redfish::Error {
-    redfish_error::one_message(Base::ResourceAtUriUnauthorized(uri.to_string(), message).into())
-}
-
-fn unauthorized_str(uri: &Uri, message: &str) -> redfish::Error {
-    unauthorized(uri, message.to_string())
+fn insufficient_privilege() -> redfish::Error {
+    redfish_error::one_message(Base::InsufficientPrivilege.into())
 }
 
 pub trait BasicAuthentication {
@@ -62,35 +58,24 @@ where
         let authorization = parts
             .headers
             .get("Authorization")
-            .ok_or_else(|| unauthorized_str(&parts.uri, "Missing Authorization header"))?
+            .ok_or_else(|| insufficient_privilege())?
             .to_str()
-            .map_err(|_| {
-                unauthorized_str(&parts.uri, "Invalid characters in Authorization header")
-            })?
+            .map_err(|_| insufficient_privilege())?
             .strip_prefix("Basic ")
-            .ok_or_else(|| unauthorized_str(&parts.uri, "Authorization header is malformed"))?
+            .ok_or_else(|| insufficient_privilege())?
             .to_string();
 
         let result = general_purpose::STANDARD
             .decode(&authorization)
-            .map_err(|_| {
-                unauthorized_str(&parts.uri, "Invalid base64 string in Authorization header")
-            })?;
+            .map_err(|_| insufficient_privilege())?;
 
         let credentials: Vec<&str> = str::from_utf8(&result)
-            .map_err(|_| {
-                unauthorized_str(&parts.uri, "Non-UTF-8 characters in Authorization header")
-            })?
+            .map_err(|_| insufficient_privilege())?
             .split(":")
             .collect();
 
         if credentials.len() != 2 {
-            return Err(unauthorized(
-                &parts.uri,
-                "Wrong number of components in Authorization string, expected 2, found "
-                    .to_string()
-                    + &credentials.len().to_string(),
-            ));
+            return Err(insufficient_privilege());
         }
 
         self.authenticator
