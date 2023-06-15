84105e654c5991aac4994e584d8d6cf7957df116
Ethan D. Twardy <ethan.twardy@gmail.com>
2023-06-14 21:04:19 -0500

    generator: Fill in a bogus implementation of the PrivilegeRegistry

--- redfish-generator/src/main/java/com/twardyece/dmtf/component/PrivilegeRegistry.java
+++ redfish-generator/src/main/java/com/twardyece/dmtf/component/PrivilegeRegistry.java
@@ -2,6 +2,7 @@ package com.twardyece.dmtf.component;
 
 import com.twardyece.dmtf.CratePath;
 import com.twardyece.dmtf.RustType;
+import com.twardyece.dmtf.text.PascalCaseName;
 import io.swagger.v3.oas.models.PathItem;
 
 import java.nio.file.Path;
@@ -9,10 +10,21 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PrivilegeRegistry {
-    public PrivilegeRegistry(Path privilegeRegistry, CratePath privilegePath) {}
+
+    private final CratePath privilegePath;
+
+    public PrivilegeRegistry(Path privilegeRegistry, CratePath privilegePath) {
+        this.privilegePath = privilegePath;
+    }
 
     public Map<PathItem.HttpMethod, RustType> getPrivilegesRequiredForComponent(String componentName) {
+        // TODO: Fill in this implementation
+        RustType login = new RustType(this.privilegePath, new PascalCaseName("Login"));
         Map<PathItem.HttpMethod, RustType> privilegeMap = new HashMap<>();
+        privilegeMap.put(PathItem.HttpMethod.GET, login);
+        privilegeMap.put(PathItem.HttpMethod.POST, login);
+        privilegeMap.put(PathItem.HttpMethod.PUT, login);
+        privilegeMap.put(PathItem.HttpMethod.PATCH, login);
         return privilegeMap;
     }
 }
