9853431ac2d4406960dff58ca6418c8dc7c7df57
Ethan D. Twardy <ethan.twardy@gmail.com>
2023-06-27 21:11:38 -0500

    seuss: Add Cache-Control header to Response
    The behavior of seuss::Response should be to set Cache-Control to
    no-cache. This is the safest default, as Redfish responses can often
    not be cached. It is possible to override this behavior if desired.
    
    This also marks the first 100% passing of the examples/simple
    application:
    
        Summary - PASS: 199, WARN: 0, FAIL: 0, NOT_TESTED: 49

--- seuss/src/model.rs
+++ seuss/src/model.rs
@@ -1,7 +1,18 @@
 use axum::{response::IntoResponse, Json};
 use redfish_codegen::{Metadata, SCHEMA_BASE_URL};
 
-pub struct Link<M>(pub &'static str, pub M);
+/// Constructs an HTTP response with the Link header set to the path of the
+/// json-schema document describing the Redfish model contained in the response
+/// body.
+pub struct Link<M>(
+    /// The base URL of the json-schema document. This can be used to refer the
+    /// json-schema client to a relative path on the current service, or (the
+    /// default behavior with the use of [crate::Response]) relative to the
+    /// public Redfish schema index.
+    pub &'static str,
+    /// The model instance forming the response body.
+    pub M,
+);
 
 impl<M> IntoResponse for Link<M>
 where
@@ -20,6 +31,12 @@ where
     }
 }
 
+/// Constructs an HTTP response using the provided Redfish model instance. This
+/// creates a request with the following:
+/// 1. The Link header is set with the URL to the json-schema of the model
+///    contained in the request body.
+/// 2. The Cache-Control header is set to "no-cache".
+/// 3. The response body is serialized to JSON.
 pub struct Response<M>(pub M);
 
 impl<M> IntoResponse for Response<M>
@@ -28,6 +45,10 @@ where
     Json<M>: IntoResponse,
 {
     fn into_response(self) -> axum::response::Response {
-        Link(SCHEMA_BASE_URL, self.0).into_response()
+        (
+            [("Cache-Control", "no-cache")],
+            Link(SCHEMA_BASE_URL, self.0),
+        )
+            .into_response()
     }
 }
