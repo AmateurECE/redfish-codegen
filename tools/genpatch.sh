66c27a1df6cae1d027907d742be711a57a4ac939
Ethan D. Twardy <ethan.twardy@gmail.com>
2023-09-28 07:17:43 -0500

    tools: Refactor genpatch.sh
    This refactor allows us to add new scripts that generate new patches.

--- tools/genpatch.sh
+++ tools/genpatch.sh
@@ -1,29 +1,54 @@
-#!/bin/sh -xe
+#!/bin/bash -xe
 # Generates some patches in the set that fixes the OpenAPI documents for code
 # generation.
 
-FIX_OPENAPI_FRAGMENTS=$(dirname $0)/fix_openapi_fragments.py
+SCRIPT_DIR=$(dirname $0)
+
+export QUILT_PC=api/.pc
+export QUILT_PATCHES=schema-patches
+
+PATCHES=(
+  "0001-Translate-openapi-fragments.patch"
+)
+
+SCRIPTS=(
+  "${SCRIPT_DIR}/fix_openapi_fragments.py"
+)
 
 if [ ! -d schema-patches ]; then
 	printf >&2 "This script should be run from the root of the repository.\n"
 	exit 1
 fi
 
-make -f generate.mk clean && make -f generate.mk unzip
+setup() {
+  make -f generate.mk clean && make -f generate.mk unzip
 
-export QUILT_PC=api/.pc
-export QUILT_PATCHES=schema-patches
+  # Reset the patches
+  for patch in $PATCHES; do
+    rm -f schema-patches/$patch
+    sed -i -e "/$patch/d" schema-patches/series
+  done
+}
 
-PATCH_NAME=0001-Translate-openapi-fragments.patch
+teardown() {
+  make -f generate.mk clean
+}
 
-# Create a new patch that will convert the fragments to valid OpenAPI
-# documents.
-rm -f schema-patches/$PATCH_NAME
-sed -i -e "/$PATCH_NAME/d" schema-patches/series
-quilt new $PATCH_NAME
-quilt add api/openapi/*.yaml
+patch_by_script() {
+  local patch=$1
+  local script=$2
 
-python3 $FIX_OPENAPI_FRAGMENTS api/openapi
+  quilt new $patch
+  quilt add api/openapi/*.yaml
 
-quilt refresh
-make -f generate.mk clean
+  python3 $script api/openapi
+
+  quilt refresh
+}
+
+setup
+for ((i = 0; i < ${#PATCHES[@]}; i++)); do
+  patch_by_script ${PATCHES[$i]} ${SCRIPTS[$i]}
+done
+teardown
+
