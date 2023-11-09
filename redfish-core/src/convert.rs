b0ad064b2586ece50895759b7df44512b59074a0
Ethan D. Twardy <ethan.twardy@gmail.com>
2023-11-09 17:33:19 -0600

    redfish-core: Add IntoEventRecord trait
    This trait takes a registry item and converts it into an EventRecord.
    It will be used to implement tracing.

--- redfish-core/src/convert.rs
+++ redfish-core/src/convert.rs
@@ -1,4 +1,4 @@
-use redfish_codegen::{models::message::v1_1_2::Message, Registry};
+use redfish_codegen::{models::{message::v1_1_2::Message, event::v1_8_0::EventRecord}, Registry};
 
 pub trait IntoRedfishMessage {
     fn into_redfish_message(self) -> Message;
@@ -13,6 +13,25 @@ where
             message_id: self.id().to_string(),
             message_severity: Some(self.severity()),
             resolution: Some(self.resolution().to_string()),
+            message: Some(self.clone().message()),
+            message_args: self.args(),
+            ..Default::default()
+        }
+    }
+}
+
+pub trait IntoEventRecord {
+    fn into_event_record(self) -> EventRecord;
+}
+
+impl<S> IntoEventRecord for S
+where
+    S: Registry<'static> + Clone,
+{
+    fn into_event_record(self) -> EventRecord {
+        EventRecord {
+            message_id: self.id().to_string(),
+            message_severity: Some(self.severity()),
             message: Some(self.clone().message()),
             message_args: self.args(),
             ..Default::default()
